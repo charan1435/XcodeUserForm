fb645e9f3ed53bb7ae7171c3829f8e5faf2a1759
charan1435 <sc16101@gmail.com>
2024-11-22 03:00:07 +0530

    Add person Logic

---
+++ Working2/AddPerson.swift
@@ -0,0 +1,18 @@
+//
+//  AddPerson.swift
+//  Working2
+//
+//  Created by Sabith Salman on 2024-11-22.
+//
+
+import SwiftUI
+
+struct AddPerson: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPerson()
+}
