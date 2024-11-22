b9e04296bb96fecc73f167b4b69a24aa240f1a8e
charan1435 <sc16101@gmail.com>
2024-11-22 13:50:24 +0530

    Setting logic for settings age

--- Working2/SettingsView.swift
+++ Working2/SettingsView.swift
@@ -8,7 +8,16 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var fontSize: Double = 20.0
     var body: some View {
+        NavigationStack{
+            VStack{
+                Slider(value: $fontSize , in: 0...100, step: 1)
+                Text("Font Size \(String(format: "%.2f", fontSize))")
+            }
+            
+                .navigationTitle("Settings")
+        }
         
     }
 }
